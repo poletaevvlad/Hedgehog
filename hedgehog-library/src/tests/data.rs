fab7fb2f53583600fdd85e1575efc0a0295825e5
Vlad Poletaev <poletaev.vladislav@gmail.com>
2021-11-29 23:22:02 +0300

    Created unit tests for updating the episodes

---
+++ hedgehog-library/src/tests/data.rs
@@ -0,0 +1,79 @@
+use crate::model::EpisodeSummary;
+use chrono::{TimeZone, Utc};
+use std::time::Duration;
+
+pub(super) struct EpisodeSummaryFixture<'a> {
+    episode_number: Option<i64>,
+    season_number: Option<i64>,
+    title: Option<&'a str>,
+    duration: Option<Duration>,
+    publication_date: Option<(i32, u32, u32, u32, u32, u32)>,
+}
+
+impl<'a> EpisodeSummaryFixture<'a> {
+    pub(super) fn assert_equals(&self, summary: &EpisodeSummary) {
+        assert_eq!(self.episode_number, summary.episode_number);
+        assert_eq!(self.season_number, summary.season_number);
+        assert_eq!(self.title, summary.title.as_deref());
+        assert_eq!(self.duration, summary.duration);
+        assert_eq!(
+            self.publication_date
+                .map(|(y, m, d, h, min, s)| Utc.ymd(y, m, d).and_hms(h, min, s),),
+            summary.publication_date
+        );
+    }
+}
+
+pub(super) mod feed1 {
+    use super::*;
+
+    pub(in super::super) const EPISODE_1: EpisodeSummaryFixture = EpisodeSummaryFixture {
+        episode_number: None,
+        season_number: None,
+        title: Some("Episode #1"),
+        duration: None,
+        publication_date: Some((2021, 12, 18, 12, 0, 0)),
+    };
+    pub(in super::super) const EPISODE_2: EpisodeSummaryFixture = EpisodeSummaryFixture {
+        episode_number: Some(2),
+        season_number: Some(1),
+        title: Some("Episode #2"),
+        duration: Some(Duration::from_secs(180)),
+        publication_date: Some((2021, 12, 19, 12, 0, 0)),
+    };
+    pub(in super::super) const EPISODE_3: EpisodeSummaryFixture = EpisodeSummaryFixture {
+        episode_number: Some(3),
+        season_number: None,
+        title: Some("Episode #3"),
+        duration: Some(Duration::from_secs(170)),
+        publication_date: Some((2021, 12, 20, 12, 0, 0)),
+    };
+    pub(in super::super) const EPISODE_3_UPDATED: EpisodeSummaryFixture = EpisodeSummaryFixture {
+        episode_number: Some(3),
+        season_number: None,
+        title: Some("Episode #3 (updated)"),
+        duration: Some(Duration::from_secs(170)),
+        publication_date: Some((2021, 12, 20, 12, 0, 0)),
+    };
+    pub(in super::super) const EPISODE_4: EpisodeSummaryFixture = EpisodeSummaryFixture {
+        episode_number: None,
+        season_number: Some(1),
+        title: Some("Episode #4"),
+        duration: Some(Duration::from_secs(160)),
+        publication_date: Some((2021, 12, 21, 12, 0, 0)),
+    };
+    pub(in super::super) const EPISODE_5: EpisodeSummaryFixture = EpisodeSummaryFixture {
+        episode_number: None,
+        season_number: None,
+        title: Some("Episode #5"),
+        duration: Some(Duration::from_secs(150)),
+        publication_date: Some((2021, 12, 22, 12, 0, 0)),
+    };
+    pub(in super::super) const EPISODE_6: EpisodeSummaryFixture = EpisodeSummaryFixture {
+        episode_number: None,
+        season_number: None,
+        title: Some("Episode #6"),
+        duration: Some(Duration::from_secs(140)),
+        publication_date: Some((2021, 12, 23, 12, 0, 0)),
+    };
+}
---
+++ hedgehog-library/src/tests/mod.rs
@@ -0,0 +1,282 @@
+#![cfg(test)]
+
+mod data;
+
+use crate::model::{EpisodeSummary, FeedError, FeedId, FeedStatus};
+use crate::sqlite::SqliteDataProvider;
+use crate::{
+    EpisodeSummariesRequest, EpisodesListMetadataRequest, EpisodesQuery, FeedSummariesRequest,
+    FeedUpdateNotification, FeedUpdateRequest, FeedUpdateResult, Library, Page,
+};
+use actix::prelude::*;
+use reqwest::StatusCode;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+
+struct NotificationListener {
+    messages: Sender<FeedUpdateNotification>,
+}
+
+impl NotificationListener {
+    fn new(sender: Sender<FeedUpdateNotification>) -> Self {
+        NotificationListener { messages: sender }
+    }
+}
+
+impl Actor for NotificationListener {
+    type Context = Context<Self>;
+}
+
+impl Handler<FeedUpdateNotification> for NotificationListener {
+    type Result = ResponseFuture<()>;
+
+    fn handle(&mut self, msg: FeedUpdateNotification, _ctx: &mut Self::Context) -> Self::Result {
+        let sender = self.messages.clone();
+        Box::pin(async move {
+            sender.send(msg).await.unwrap();
+        })
+    }
+}
+
+async fn create_library() -> (Addr<Library>, Receiver<FeedUpdateNotification>) {
+    let provider = SqliteDataProvider::connect(":memory:").unwrap();
+    let library = Library::new(provider).start();
+    let (sender, reciever) = channel(16);
+    let notifications = NotificationListener::new(sender).start();
+    let msg = FeedUpdateRequest::Subscribe(notifications.recipient());
+    library.send(msg).await.unwrap();
+    (library, reciever)
+}
+
+macro_rules! let_assert {
+    (let $first:ident$(::$tail:ident)* ($($var:ident),*) = $value:expr) => {
+       let ($($var,)*) = match $value {
+           $first$(::$tail)* ($($var),*) => ($($var,)*),
+           msg => panic!("wrong variant: {:?}", msg),
+       };
+    };
+}
+
+#[actix::test]
+async fn adding_new_feed() {
+    let mock_server = httpmock::MockServer::start();
+    mock_server.mock(|when, then| {
+        when.method(httpmock::Method::GET).path("/feed.xml");
+        then.status(200)
+            .header("content-type", "application/xml")
+            .body(include_str!("../test_data/feed1.xml"));
+    });
+    let (library, mut reciever) = create_library().await;
+
+    let summaries = library.send(FeedSummariesRequest).await.unwrap().unwrap();
+    assert_eq!(summaries.len(), 0);
+
+    let source_url = format!("{}/feed.xml", mock_server.base_url());
+    let msg = FeedUpdateRequest::AddFeed(source_url.clone());
+    library.send(msg).await.unwrap();
+
+    let feed_added = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::FeedAdded(summary) = feed_added);
+    assert_eq!(summary.title, source_url);
+    assert!(!summary.has_title);
+    assert_eq!(summary.status, FeedStatus::Pending);
+
+    let feed_update_started = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::UpdateStarted(feed_ids) = feed_update_started);
+    assert_eq!(feed_ids, vec![summary.id]);
+
+    let update_finished = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::UpdateFinished(feed_id, update_result) = update_finished);
+    assert_eq!(feed_id, summary.id);
+    let_assert!(let FeedUpdateResult::Updated(updated_summary) = update_result);
+    assert_eq!(updated_summary.id, summary.id);
+    assert_eq!(updated_summary.title, "Sample Podcast");
+    assert!(updated_summary.has_title);
+    assert_eq!(updated_summary.status, FeedStatus::Loaded);
+
+    let summaries = library.send(FeedSummariesRequest).await.unwrap().unwrap();
+    assert_eq!(summaries.len(), 1);
+    assert_eq!(summaries[0], updated_summary);
+}
+
+#[actix::test]
+async fn adding_new_feed_error() {
+    let mock_server = httpmock::MockServer::start();
+    mock_server.mock(|when, then| {
+        when.method(httpmock::Method::GET).path("/feed.xml");
+        then.status(404);
+    });
+    let (library, mut reciever) = create_library().await;
+
+    let source_url = format!("{}/feed.xml", mock_server.base_url());
+    let msg = FeedUpdateRequest::AddFeed(source_url.clone());
+    library.send(msg).await.unwrap();
+
+    let feed_added = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::FeedAdded(summary) = feed_added);
+
+    let feed_update_started = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::UpdateStarted(_feed_ids) = feed_update_started);
+
+    let update_finished = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::UpdateFinished(feed_id, update_result) = update_finished);
+    assert_eq!(feed_id, summary.id);
+    let_assert!(let FeedUpdateResult::StatusChanged(status) = update_result);
+    let_assert!(let FeedStatus::Error(error) = status);
+    assert_eq!(
+        error,
+        FeedError::HttpError(StatusCode::from_u16(404).unwrap())
+    );
+
+    let summaries = library.send(FeedSummariesRequest).await.unwrap().unwrap();
+    assert_eq!(summaries.len(), 1);
+    assert_eq!(summaries[0].id, summary.id);
+    assert_eq!(summaries[0].title, source_url);
+    assert!(!summaries[0].has_title);
+    assert_eq!(summaries[0].status, status);
+}
+
+async fn seed_feed(
+    server: &httpmock::MockServer,
+    library: Addr<Library>,
+    reciever: &mut Receiver<FeedUpdateNotification>,
+    xml: &str,
+) -> FeedId {
+    let mut mock = server.mock(|when, then| {
+        when.method(httpmock::Method::GET).path("/feed.xml");
+        then.status(200).body(xml);
+    });
+
+    let source_url = format!("{}/feed.xml", server.base_url());
+    let msg = FeedUpdateRequest::AddFeed(source_url.clone());
+    library.send(msg).await.unwrap();
+
+    loop {
+        let msg = reciever.recv().await.unwrap();
+        if let FeedUpdateNotification::UpdateFinished(feed_id, _) = msg {
+            mock.delete();
+            return feed_id;
+        }
+    }
+}
+
+async fn get_episode_summaries(
+    library: Addr<Library>,
+    query: EpisodesQuery,
+) -> Vec<EpisodeSummary> {
+    let list_metadata = library
+        .send(EpisodesListMetadataRequest(query.clone()))
+        .await
+        .unwrap()
+        .unwrap();
+
+    let mut episodes = Vec::with_capacity(list_metadata.items_count);
+    let mut index = 0;
+    while index * 2 < list_metadata.items_count {
+        let page = library
+            .send(EpisodeSummariesRequest::new(
+                query.clone(),
+                Page { index, size: 2 },
+            ))
+            .await
+            .unwrap()
+            .unwrap();
+        assert!(page.len() <= 2);
+        episodes.extend(page);
+        index += 1;
+    }
+
+    let empty_page = library
+        .send(EpisodeSummariesRequest::new(
+            query.clone(),
+            Page { index, size: 2 },
+        ))
+        .await
+        .unwrap()
+        .unwrap();
+    assert!(empty_page.is_empty());
+
+    assert_eq!(
+        list_metadata.max_season_number,
+        episodes.iter().filter_map(|ep| ep.season_number).max(),
+    );
+    assert_eq!(
+        list_metadata.max_episode_number,
+        episodes.iter().filter_map(|ep| ep.episode_number).max(),
+    );
+    assert_eq!(
+        list_metadata.max_duration,
+        episodes.iter().filter_map(|ep| ep.duration).max(),
+    );
+
+    episodes
+}
+
+#[actix::test]
+async fn creates_episodes() {
+    let (library, mut reciever) = create_library().await;
+    let feed = include_str!("../test_data/feed1.xml");
+    let mock_server = httpmock::MockServer::start();
+    let feed_id = seed_feed(&mock_server, library.clone(), &mut reciever, feed).await;
+
+    let query = EpisodesQuery::Multiple {
+        feed_id: Some(feed_id),
+    };
+    let episodes = get_episode_summaries(library, query).await;
+    assert!(episodes.iter().all(|ep| ep.feed_id == feed_id));
+    let expected = [
+        data::feed1::EPISODE_5,
+        data::feed1::EPISODE_4,
+        data::feed1::EPISODE_3,
+        data::feed1::EPISODE_2,
+        data::feed1::EPISODE_1,
+    ];
+    assert_eq!(episodes.len(), expected.len());
+    for (expected, actual) in expected.iter().zip(episodes.iter()) {
+        expected.assert_equals(actual);
+    }
+}
+
+#[actix::test]
+async fn updates_episodes_on_update() {
+    let (library, mut reciever) = create_library().await;
+    let feed = include_str!("../test_data/feed1.xml");
+    let mock_server = httpmock::MockServer::start();
+    let feed_id = seed_feed(&mock_server, library.clone(), &mut reciever, feed).await;
+    mock_server.mock(|when, then| {
+        when.method(httpmock::Method::GET).path("/feed.xml");
+        then.status(200)
+            .body(include_str!("../test_data/feed1_updated_episodes.xml"));
+    });
+
+    let msg = FeedUpdateRequest::UpdateSingle(feed_id);
+    library.send(msg).await.unwrap();
+
+    let update_started = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::UpdateStarted(ids) = update_started);
+    assert_eq!(ids, vec![feed_id]);
+
+    let update_started = reciever.recv().await.unwrap();
+    let_assert!(let FeedUpdateNotification::UpdateFinished(id, update) = update_started);
+    assert_eq!(id, feed_id);
+    let_assert!(let FeedUpdateResult::Updated(summary) = update);
+    assert_eq!(summary.id, feed_id);
+    assert_eq!(&summary.title, "Sample Podcast");
+
+    let query = EpisodesQuery::Multiple {
+        feed_id: Some(feed_id),
+    };
+    let episodes = get_episode_summaries(library, query).await;
+    assert!(episodes.iter().all(|ep| ep.feed_id == feed_id));
+    let expected = [
+        data::feed1::EPISODE_6,
+        data::feed1::EPISODE_5,
+        data::feed1::EPISODE_4,
+        data::feed1::EPISODE_3_UPDATED,
+        data::feed1::EPISODE_2,
+        data::feed1::EPISODE_1,
+    ];
+    assert_eq!(episodes.len(), expected.len());
+    for (expected, actual) in expected.iter().zip(episodes.iter()) {
+        expected.assert_equals(actual);
+    }
+}

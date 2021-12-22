68d5ae35eef1d0ddf184097f0d5957956d165816
Vlad Poletaev <poletaev.vladislav@gmail.com>
2021-12-23 02:33:28 +0300

    Implemented parsing OPML podcast feeds

--- hedgehog-library/src/opml.rs
+++ hedgehog-library/src/opml.rs
@@ -10,6 +10,12 @@ pub enum Error {
 
     #[error(transparent)]
     Db(#[from] QueryError),
+
+    #[error("The file is not a valid OPML")]
+    NotOpmlFile,
+
+    #[error(transparent)]
+    Utf8(#[from] std::str::Utf8Error),
 }
 
 pub fn build_opml<W: io::Write, D: DataProvider>(write: W, data: &D) -> Result<(), Error> {
@@ -44,7 +50,7 @@ pub fn build_opml<W: io::Write, D: DataProvider>(write: W, data: &D) -> Result<(
                 attrs.push((b"title", title));
                 attrs.push((b"text", title));
             }
-            let element = BytesStart::borrowed_name(b"ouline").with_attributes(attrs);
+            let element = BytesStart::borrowed_name(b"outline").with_attributes(attrs);
             writer.write_event(Event::Empty(element))?;
         }
         writer.write_event(Event::End(BytesEnd::borrowed(b"body")))?;
@@ -54,12 +60,139 @@ pub fn build_opml<W: io::Write, D: DataProvider>(write: W, data: &D) -> Result<(
     Ok(())
 }
 
+pub fn parse_opml<R: io::BufRead>(reader: R) -> Result<OpmlEntries<R>, Error> {
+    let mut reader = quick_xml::Reader::from_reader(reader);
+    let mut buf = Vec::new();
+    let mut depth: u64 = 0;
+    loop {
+        let event = reader.read_event(&mut buf)?;
+
+        let (bytes_start, is_empty) = match event {
+            Event::Start(start) => (start, false),
+            Event::End(_) => {
+                depth = depth.saturating_sub(1);
+                continue;
+            }
+            Event::Empty(start) => (start, true),
+            Event::Eof => return Err(Error::NotOpmlFile),
+            _ => continue,
+        };
+
+        if depth == 0 && bytes_start.name() != b"opml" {
+            return Err(Error::NotOpmlFile);
+        } else if depth == 1 && bytes_start.name() == b"body" {
+            return Ok(OpmlEntries {
+                reader: if is_empty { None } else { Some(reader) },
+                depth: 0,
+                buf,
+            });
+        }
+
+        if !is_empty {
+            depth += 1;
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct OpmlEntry {
+    title: Option<String>,
+    html_feed: Option<String>,
+    xml_feed: String,
+}
+
+pub struct OpmlEntries<R: io::BufRead> {
+    reader: Option<quick_xml::Reader<R>>,
+    depth: u64,
+    buf: Vec<u8>,
+}
+
+impl<R: io::BufRead> Iterator for OpmlEntries<R> {
+    type Item = Result<OpmlEntry, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let event = match self.reader {
+                Some(ref mut reader) => reader.read_event(&mut self.buf),
+                None => return None,
+            };
+            let bytes_start = match event {
+                Ok(Event::Start(start)) => {
+                    self.depth += 1;
+                    start
+                }
+                Ok(Event::End(_)) => {
+                    if self.depth == 0 {
+                        return None;
+                    }
+                    self.depth -= 1;
+                    continue;
+                }
+                Ok(Event::Empty(start)) => start,
+                Ok(_) => continue,
+                Err(err) => return Some(Err(err.into())),
+            };
+
+            if bytes_start.name() == b"outline" {
+                let mut title = None;
+                let mut html_feed = None;
+                let mut xml_feed = None;
+                let mut is_rss = false;
+                for attr in bytes_start.attributes() {
+                    let attr = match attr {
+                        Ok(attr) => attr,
+                        Err(err) => return Some(Err(err.into())),
+                    };
+                    let value = match attr.unescaped_value() {
+                        Ok(value) => value,
+                        Err(err) => return Some(Err(err.into())),
+                    };
+                    match attr.key {
+                        b"type" => is_rss = (&*value) == (b"rss" as &[u8]),
+                        b"xmlUrl" => {
+                            xml_feed = match std::str::from_utf8(&value) {
+                                Ok(xml_feed) => Some(xml_feed.to_string()),
+                                Err(error) => return Some(Err(error.into())),
+                            }
+                        }
+                        b"htmlUrl" => {
+                            html_feed = match std::str::from_utf8(&value) {
+                                Ok(html_feed) => Some(html_feed.to_string()),
+                                Err(error) => return Some(Err(error.into())),
+                            }
+                        }
+                        b"title" | b"text" => {
+                            title = match std::str::from_utf8(&value) {
+                                Ok(title) => Some(title.to_string()),
+                                Err(error) => return Some(Err(error.into())),
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+
+                if !is_rss {
+                    continue;
+                }
+                if let Some(xml_feed) = xml_feed {
+                    return Some(Ok(OpmlEntry {
+                        title,
+                        html_feed,
+                        xml_feed,
+                    }));
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::build_opml;
     use crate::{
         datasource::{DataProvider, EpisodeWriter, WritableDataProvider},
         metadata::FeedMetadata,
+        opml::{parse_opml, OpmlEntry},
         SqliteDataProvider,
     };
     use std::io::Cursor;
@@ -117,5 +250,57 @@ mod tests {
 
         let xml = String::from_utf8(buffer).unwrap();
         assert_eq!(&xml, include_str!("./test_data/with_feeds.opml").trim_end());
+    }
+
+    #[test]
+    fn parse_opml_empty() {
+        let reader = Cursor::new(include_str!("./test_data/empty.opml"));
+        let mut parser = parse_opml(reader).unwrap();
+        assert!(parser.next().is_none());
+    }
+
+    #[test]
+    fn parse_ompl_non_empty() {
+        let reader = Cursor::new(include_str!("./test_data/with_feeds.opml"));
+        let mut parser = parse_opml(reader).unwrap();
+        assert_eq!(
+            parser.next().unwrap().unwrap(),
+            OpmlEntry {
+                title: None,
+                html_feed: None,
+                xml_feed: "https://example.com/source_not_fetched".to_string(),
+            }
+        );
+        assert_eq!(
+            parser.next().unwrap().unwrap(),
+            OpmlEntry {
+                title: Some("Feed title".to_string()),
+                html_feed: Some("http://example.com/podcast2.html".to_string()),
+                xml_feed: "https://example.com/source_2".to_string(),
+            }
+        );
+        assert_eq!(
+            parser.next().unwrap().unwrap(),
+            OpmlEntry {
+                title: Some("\"Second\" <fetched> podcast".to_string()),
+                html_feed: Some("http://example.com/podcast3.html".to_string()),
+                xml_feed: "https://example.com/source_3".to_string(),
+            }
+        );
+    }
+
+    #[test]
+    fn parse_ompl_with_invalid_nodes() {
+        let reader = Cursor::new(include_str!("./test_data/with_invalid_nodes.opml"));
+        let parser = parse_opml(reader).unwrap();
+        let resource: Vec<String> = parser.map(|entry| entry.unwrap().xml_feed).collect();
+        assert_eq!(
+            resource,
+            vec![
+                "https://example.com/source_1".to_string(),
+                "https://example.com/source_2".to_string(),
+                "https://example.com/source_3".to_string()
+            ]
+        );
     }
 }

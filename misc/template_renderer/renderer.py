0e3ec3d5a3dde291475eb9f3859446b5af258442
Vlad Poletaev <poletaev.vladislav@gmail.com>
2021-12-30 00:38:40 +0300

    Created a color_mix function for jinja2 template

--- misc/template_renderer/renderer.py
+++ misc/template_renderer/renderer.py
@@ -2,8 +2,25 @@
 
 import sys
 from pathlib import Path
+from typing import Tuple
 
+import toml
 from jinja2 import DictLoader, Environment, select_autoescape
+
+
+def parse_color(color: str) -> Tuple[int, int, int]:
+    color_int = int(color, 16)
+    return (color_int >> 16) & 0xFF, (color_int >> 8) & 0xFF, color_int & 0xFF
+
+
+def color_mix(fg: str, bg: str, factor: float) -> str:
+    fg_channels = parse_color(fg)
+    bg_channels = parse_color(bg)
+    red, green, blue = (
+        int(ch1 * factor + ch2 * (1 - factor))
+        for ch1, ch2 in zip(bg_channels, fg_channels)
+    )
+    return f"{red:02x}{green:02x}{blue:02x}"
 
 
 def main():
@@ -16,8 +33,11 @@ def main():
         loader=DictLoader({"index": template}),
         autoescape=select_autoescape(default=False, default_for_string=False),
     )
+    jinja_env.globals["color_mix"] = color_mix
+
     template = jinja_env.get_template("index")
-    print(template.render(dict()))
+    context = toml.load(sys.stdin)
+    print(template.render(context))
 
 
 if __name__ == "__main__":

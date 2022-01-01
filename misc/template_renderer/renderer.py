1c51347da792dab099888ef4af7a3bfd3b377987
Vlad Poletaev <poletaev.vladislav@gmail.com>
2022-01-02 00:43:01 +0300

    Added a header to generated theme definitions

--- misc/template_renderer/renderer.py
+++ misc/template_renderer/renderer.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Tuple
 
 import toml
-from jinja2 import DictLoader, Environment, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def parse_color(color: str) -> Tuple[int, int, int]:
@@ -23,29 +23,39 @@ def color_mix(fg: str, bg: str, factor: float) -> str:
     return f"{red:02x}{green:02x}{blue:02x}"
 
 
-def main():     
+def main():
     if len(sys.argv) < 2:
         print(f"USAGE: {sys.argv[0]} (<var>=<value>)* <template path>", file=sys.stderr)
         sys.exit(1)
 
-    template = Path(sys.argv[-1]).read_text()
+    path = Path(sys.argv[-1]).resolve()
     jinja_env = Environment(
-        loader=DictLoader({"index": template}),
+        loader=FileSystemLoader(str(path.parent)),
         autoescape=select_autoescape(default=False, default_for_string=False),
     )
     jinja_env.globals["color_mix"] = color_mix
 
-    template = jinja_env.get_template("index")
+    template = jinja_env.get_template(path.name)
 
     context = toml.load(sys.stdin)
     for values in sys.argv[1:-1]:
         key_value = values.split("=", maxsplit=2)
         if len(key_value) != 2:
             print(f"Invalid key-value pair '{key_value}'\n", file=sys.stderr)
-            print(f"USAGE: {sys.argv[0]} (<var>=<value>)* <template path>", file=sys.stderr)
+            print(
+                f"USAGE: {sys.argv[0]} (<var>=<value>)* <template path>",
+                file=sys.stderr,
+            )
             sys.exit(1)
         key, value = key_value
         context[key] = value
+
+    repo_root = path.parent
+    while repo_root != repo_root.root:
+        if (repo_root / ".git").is_dir():
+            break
+        repo_root = repo_root.parent
+    context["_template"] = path.relative_to(repo_root)
 
     print(template.render(context))
 

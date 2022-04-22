e7fe76c240deaa93cfcf71ef0dab9a68adc87839
Vlad Poletaev <poletaev.vladislav@gmail.com>
2022-04-22 03:16:21 +0300

    Injecting the version number into the Windows archive name

--- build-win.bat
+++ build-win.bat
@@ -9,14 +9,14 @@ if not exist target mkdir target
 
 cargo build --target %arch%-pc-windows-msvc --release --message-format=json-render-diagnostics | jq -r "select(.out_dir) | select(.package_id | startswith(\"hedgehog-tui \")) | .out_dir" > .\target\out_dir_path_%arch%
 
-rmdir /S /Q .\build\hedgehog-0.1.0-windows-%arch%
-mkdir .\build\hedgehog-0.1.0-windows-%arch%
+rmdir /S /Q .\build\hedgehog-current-windows-%arch%
+mkdir .\build\hedgehog-current-windows-%arch%
 
-copy target\%arch%-pc-windows-msvc\release\hedgehog.exe build\hedgehog-0.1.0-windows-%arch%\
-for /f "usebackq tokens=*" %%a in (`type .\target\out_dir_path_%arch%`) do xcopy /S /I %%a\config build\hedgehog-0.1.0-windows-%arch%\config
+copy target\%arch%-pc-windows-msvc\release\hedgehog.exe build\hedgehog-current-windows-%arch%\
+for /f "usebackq tokens=*" %%a in (`type .\target\out_dir_path_%arch%`) do xcopy /S /I %%a\config build\hedgehog-current-windows-%arch%\config
 
 if "%2"=="/A" (
     cd build
-	7z a -sdel hedgehog-0.1.0-windows-%arch%.zip hedgehog-0.1.0-windows-%arch%
+	7z a -sdel hedgehog-current-windows-%arch%.zip hedgehog-current-windows-%arch%
 	cd ..
 )
